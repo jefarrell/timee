11e18ddee232e6fa463628988ed1c816d3eae0e2
John Farrell <johnfarrell@172-16-250-215.DYNAPOOL.NYU.EDU>
2015-04-13 23:41:56 -0400

    server updates

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var app = express();
 var PythonShell = require('python-shell');
@@ -12,22 +11,45 @@ var options = {
 
 
 var server = app.listen(3000, function () {
-
   var host = server.address().address;
   var port = server.address().port;
   console.log('app listening at http://%s:%s', host, port);
-
 });
 
 
+var newresults;
 app.get('/calendar', function (req, res) {
   res.send('Calendar response');
   getAllEventsChoreo.execute(
 	    getAllEventsInputs,
-	    function(results){console.log(results.get_Response());},
-	    function(error){console.log(error.type); console.log(error.message);}
+	    function(results){
+	    	newresults = results.get_Response();
+	    	calendarResults(newresults);
+	    },
+	    function(error){
+	    	console.log(error.type);
+	    	console.log(error.message);
+	    }
 	);
+
 });
+
+function calendarResults(results) {
+	obj = JSON.parse(results); 
+
+	// for (var i=0;i<obj['items'].length;i++) {
+	// 	// console.log(obj['items'][i]['start']);
+	// 	console.log('hi');
+	// }
+
+	var date1 = Date.parse(obj.items[0].start.dateTime);
+	var date2 = Date.parse(obj.items[1].start.dateTime);
+	console.log(date1 < date2);
+	
+}
+
+
+
 
 app.get('/train', function (req, res) {
 	res.send('train response');
@@ -35,6 +57,30 @@ app.get('/train', function (req, res) {
 	scraper.on('message', function(message){
 		console.log(message);
   	})
+});
+
+
+var phoneInfo;
+
+app.get('/info/:info', function (request, response) {
+	phoneInfo  = request.params.name;
+	
+	//send a response to the client:
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write("You sent me: " + phoneInfo);
+	response.end();
+	console.log(phoneInfo);
+});
+
+app.get('/phoneInfo', function (req, res) {
+	 var placeholder
+	 if (!phoneInfo) {
+	 	placeholder = "no info";
+	 } else {
+	 	placeholder = phoneInfo
+	 }
+	 res.send(placeholder);
+	 console.log("info: " + placeholder)
 });
 
 
