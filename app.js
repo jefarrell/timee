8be7be997cecc6560d86ff88a250ab99a094a8da
John Farrell <johnfarrell@172-16-252-3.DYNAPOOL.NYU.EDU>
2015-05-06 02:18:06 -0400

    more sets more gets

--- app.js
+++ app.js
@@ -64,17 +64,7 @@ app.get('/phoneInfo', function (req, res) {
 	 // parse the phone info here? 
 	 	var splitter = phoneInfo.split(";");
 	 	console.log("split: " + splitter);
-	 	app.set('phoneInfo', phoneInfo);
-
-
-
-
-
-	 // phoneTest = '15:00;40;7';
-	 // var splitter = phoneTest.split(";")
-	 // app.set('phoneInfo', splitter[2]);
-	 // console.log(splitter[1]);
-	 	
+	 	app.set('phoneInfo', phoneInfo);	 	
 });
 
 
@@ -95,7 +85,7 @@ app.get('/calendar', function (req, res) {
   			var calTime = tempTime.split('-')[0].split(':')[0]-1;
   			console.log("next calendar time : " + calTime);
 
-  		/////  test parsing pseudocode  /////
+
 	    	var phonePlaceholder = req.app.get('phoneInfo');
 	    	var placeholder2 = JSON.stringify(phonePlaceholder);
 	    	var fromPhone = placeholder2.split(',');
@@ -110,7 +100,7 @@ app.get('/calendar', function (req, res) {
 	    	console.log("train: " + trainLine);
 	    	app.set('trainName', trainLine);
 	    	// Compare and send the times here;
-	    	// should I parse the phone info here?
+
 
 	    	//app.set earliest time 
 	    },
@@ -142,22 +132,14 @@ function weathertest(lat,lon, callback) {
 }
 
 
-// Need to update with info from phone
-/////  CHECK THE MTA  /////
-var tester = app.get('phoneInfo');
-
-
-
-
-
 /////  MTA WEBSCRAPER  /////
 function trainScraper(callback) {
 	var placeholder = app.get('phoneInfo');
 	var userTrain = placeholder.split(",")[4];
 	console.log("trainscraper train: " + userTrain);
 	var options = {
-	mode : 'json',
-	args: userTrain
+		mode : 'json',
+		args: userTrain
 }
 
 	var scraper = new PythonShell('scrape.py', options);
@@ -179,23 +161,24 @@ function delayCheck(lat,lon) {
 		console.log("status title: " + message);
 		var match = "Current Status: Good Service";
 		if (message.toLowerCase() == match.toLowerCase()) {
-		// if (status === match) {
 			console.log("no train delay");
 			delays[0] = 0;
-
+			app.set('trainStatus', 0);
 		} else {
-
 			var delayMessage = status['title']
 			console.log("train delay: " + delayMessage);
+			app.set('trainStatus',1);
 		}
 	})
 
 	weathertest(lat,lon,function(probability){
 		if (probability > 0.75) {
 			delays[1] = 1;
+			app.set('weatherStatus', 1);
 			console.log("weather delay");
 		} else { 
 			delays[1] = 0;
+			app.set('weatherStatus',0);
 			console.log("no weather delay");
 		}
  	});
@@ -207,13 +190,11 @@ function delayCheck(lat,lon) {
 
 
 app.get('/delay',function (req, res) {
-	// Need to update with info from phone
 	var hold = req.app.get('phoneInfo')
 	holdParse = hold.split(",");
 	var lat = holdParse[2];
 	var lon = holdParse[3];
 
-	//delayCheck(40.7127,-74.0059);
 	delayCheck(lat,lon);
 	res.end();
 });
@@ -222,23 +203,28 @@ app.get('/delay',function (req, res) {
 /////  ROUTE FOR ARDUINO YUN  /////
 app.get('/arduinoCheck', function (req, res) {
 	res.send('request from arduino');
-	//delayCheck(40.7127,-74.0059);
-	// delayCheck(40.7127,-74.0059,function(delays){
-	// 	console.log("delays test: " + delays);
-	// });
-	if (delays[0] =! 0) {
-		delays[0] = 1;
-		
-	}
-	
+
+	var weatherStatus = req.app.get('weatherStatus');
+	console.log("weatherStatus: " + weatherStatus);
+
+	var trainStatus = req.app.get('trainStatus');
+	console.log("statusTest: " + weatherStatus);
 });
+
+
+
+
+
 
 /////  ROUTE FOR PHONE APP  /////
 app.get('/phoneCheck', function (req,res){
 	res.send('request from phone');
 	// delayCheck(40.7127,-74.0059);
-	console.log("delays: " + delays);
+
 })
+
+
+
 
 
 
